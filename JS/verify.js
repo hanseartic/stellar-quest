4908936847f9930535666f931c2a918b323aa1ac
Paul Schütte <paul.schuett@otto.de>
2021-05-31 20:55:59 +0200

    Make verify EP return 404 on unknown quests

--- JS/verify.js
+++ JS/verify.js
@@ -7,13 +7,19 @@ app.get('/verify/:quest/:account_id', (req, res) => {
     const accountId = req.params.account_id;
     const quest = req.params.quest;
     const setNo = parseInt(quest.match(/SQ(\d{2})(\d{2})/)[1]);
-    const { verify } = require(`./Set${setNo}/${quest}`);
-    process.stdout.write(`Verifying ${quest} for account ${accountId}: `);
-    return verify(accountId)
-        .then(result => {
-            process.stdout.write(result+"\n");
-            return res.json({ account: accountId, verified: result});
-        });
+    try {
+        const { verify } = require(`./Set${setNo}/${quest}`);
+        process.stdout.write(`Verifying ${quest} for account ${accountId}: `);
+        return verify(accountId)
+            .then(result => {
+                process.stdout.write(result+"\n");
+                return res.json({ account: accountId, verified: result});
+            });
+    } catch (err) {
+        process.stdout.write("Quest does not exist\n");
+        res.statusCode = 404;
+        return res.send(`Quest ${quest} not found.`);
+    }
 });
 
 app.listen(port, () => {
